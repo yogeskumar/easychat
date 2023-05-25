1dba5e34eea63074768beadf3bee3ae28abd87de
Yogesh Kumar <admin@copyassignment.com>
2023-05-25 09:40:29 +0530

    backend url updated

--- client/src/authPage.js
+++ client/src/authPage.js
@@ -11,7 +11,7 @@ const AuthPage = (props) => {
     const onLogin = (e) => {
         e.preventDefault();
         axios
-            .post("http://localhost:3001/login", { username, secret })
+            .post("https://easychat-back.onrender.com/login", { username, secret })
             .then((r) => props.onAuth({ ...r.data, secret })) // NOTE: over-ride secret
             .catch((e) => console.log(JSON.stringify(e.response.data)));
     };
@@ -19,7 +19,7 @@ const AuthPage = (props) => {
     const onSignup = (e) => {
         e.preventDefault();
         axios
-            .post("http://localhost:3001/signup", {
+            .post("https://easychat-back.onrender.com/signup", {
                 username,
                 secret,
                 email,
